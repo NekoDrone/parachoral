5c383f5b2469b3c93eea07f9877f2d9feb5c5b6d
serenity <98162476+NekoDrone@users.noreply.github.com>
2026-07-25 00:55:12 +0800

    types: refine schema

--- src/lib/types/data/releases.ts
+++ src/lib/types/data/releases.ts
@@ -1,11 +1,13 @@
 import { z } from "zod";
 
-export const Releases = z.array(
-    z.object({
-        slug: z.string(),
-        name: z.string(),
-        year: z.number().int(),
-    }),
-);
+export const Release = z.object({
+    slug: z.string(),
+    name: z.string(),
+    year: z.number().int(),
+});
+
+export type Release = z.infer<typeof Release>;
+
+export const Releases = z.array(Release);
 
 export type Releases = z.infer<typeof Releases>;
