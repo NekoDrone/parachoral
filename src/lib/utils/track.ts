3bfb28b1dbc5ad67ebafab861a8a59b824d3bbe9
serenity <98162476+NekoDrone@users.noreply.github.com>
2026-07-25 01:46:52 +0800

    utils: track path to release helper

---
+++ src/lib/utils/track.ts
@@ -0,0 +1,18 @@
+import { releasesParsed } from "#/lib/load";
+import { err, ok } from "#/lib/result";
+import type { Result } from "#/lib/result";
+import type { Release } from "#/lib/types/data/releases";
+
+/**
+ * Given the relative path to a track's YAML file, return the release that the track is filed under.
+ */
+export const trackReleaseFromPath = (path: string): Result<Release, string> => {
+    const arr = path.split("/");
+    const releaseString = arr[arr.length - 2];
+    const res = releasesParsed.find((v) => v.slug === releaseString);
+    if (!res)
+        return err(
+            `could not find given track's release slug, received ${path}`,
+        );
+    return ok(res);
+};
