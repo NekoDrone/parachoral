0d46fc80d9f57c7e83224eae89c5967a6740e020
serenity <98162476+NekoDrone@users.noreply.github.com>
2026-07-26 23:21:49 +0800

    providers: motion config

--- src/lib/providers/index.tsx
+++ src/lib/providers/index.tsx
@@ -1,4 +1,5 @@
 import { IconContext } from "@phosphor-icons/react";
+import { MotionConfig } from "motion/react";
 import type { ReactNode } from "react";
 
 export const Providers = ({ children }: { children: ReactNode }) => {
@@ -6,6 +7,7 @@ export const Providers = ({ children }: { children: ReactNode }) => {
         <IconContext.Provider
             value={{ size: 16, weight: "light", color: "currentColor" }}
         >
+            <MotionConfig reducedMotion="user" />
             {children}
         </IconContext.Provider>
     );
