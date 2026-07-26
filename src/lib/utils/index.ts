c3888667816930d208ae2367c3600a615d821972
serenity <98162476+NekoDrone@users.noreply.github.com>
2026-07-26 23:21:58 +0800

    utils: enumify helper

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,3 +1,18 @@
+/**
+ * If given an object's `typeof`, turns it into an enum-like type.
+ * e.g.
+ *```typescript
+ * const LogLevel = {
+ *   DEBUG: "DEBUG",
+ *   WARNING: "WARNING",
+ *   ERROR: "ERROR",
+ * } as const
+ * type LogLevel = Enumify<typeof LogLevel>;
+ * ```
+ * Which is a better way (imo) of declaring a typescript enum.
+ */
+export type Enumify<T> = T[keyof T];
+
 export const toRomanYear = (date: Date): string => {
     const numerals: Array<[number, string]> = [
         [1000, "M"],
