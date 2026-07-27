79d8367602c6f6ad88af6221f525c3e623f00920
serenity <98162476+NekoDrone@users.noreply.github.com>
2026-07-27 19:11:06 +0800

    track: better error rendering

--- src/routes/track/$trackSlug.tsx
+++ src/routes/track/$trackSlug.tsx
@@ -1,13 +1,55 @@
 import { getTrackBySlug } from "#/lib/data/get-track-by-slug";
+import { tracksMap } from "#/lib/load";
 import { createFileRoute, notFound } from "@tanstack/react-router";
+import { string } from "zod";
 
 export const Route = createFileRoute("/track/$trackSlug")({
     loader: ({ params }) => {
         const track = getTrackBySlug(params.trackSlug);
-        if (!track.ok) throw notFound();
+        if (!track.ok)
+            throw notFound({
+                data: {
+                    err: track.error,
+                    slug: params.trackSlug,
+                    map: tracksMap,
+                },
+            });
         return track.value;
     },
-    notFoundComponent: () => <p>No track with that name in the songbook.</p>,
+    notFoundComponent: (e) => {
+        if (
+            e.data === undefined ||
+            e.data === null ||
+            !(typeof e.data === "object" && "err" in e.data) ||
+            !(typeof e.data === "object" && "slug" in e.data) ||
+            !(typeof e.data === "object" && "map" in e.data)
+        )
+            throw new Error(
+                "Track not found but no data to support it. Something went very wrong.",
+            );
+
+        const { data } = e;
+        const { err, slug, map } = data;
+
+        if (typeof err !== "string")
+            throw new Error("mismatched error data from track slug lookup.");
+
+        if (typeof slug !== "string")
+            throw new Error("mismatched slug data from track slug lookup.");
+
+        if (!(map instanceof Map))
+            throw new Error("mismatched map data from track slug lookup.");
+
+        return (
+            <div>
+                <p>No track with that name in the songbook.</p>
+                <p>Error details:</p>
+                <p>err: {err}</p>
+                <p>slug: {slug}</p>
+                <p>map: {JSON.stringify(Array.from(map.entries()))}</p>
+            </div>
+        );
+    },
     component: RouteComponent,
 });
 
