973800485d481a9f9ecb0b7e31107182bfdc19a5
serenity <98162476+NekoDrone@users.noreply.github.com>
2026-08-06 21:09:19 +0800

    track: highlight origin track

--- src/components/tracks/TrackTimeline.tsx
+++ src/components/tracks/TrackTimeline.tsx
@@ -3,8 +3,9 @@ import { SectionHead } from "#/components/misc/SectionHead";
 const testStuff: Array<{
     label: string;
     timestamp: { start: number; end?: number };
+    origin?: boolean;
 }> = [
-        { label: "The Dawning", timestamp: { start: 0, end: 16 } },
+        { label: "The Dawning", timestamp: { start: 0, end: 16 }, origin: true },
         { label: "The Guardian", timestamp: { start: 52 } },
         { label: "Be Brave", timestamp: { start: 84, end: 107 } },
         { label: "The Dawning", timestamp: { start: 132, end: 151 } },
@@ -16,9 +17,20 @@ export const TrackTimeline = () => {
             <SectionHead label="The Score" num={1} />
             <div className="flex flex-col w-full items-center">
                 {testStuff.map((m) => (
-                    <div className="flex gap-2 justify-between w-[90%] h-12 items-center">
+                    <div
+                        className={
+                            "flex gap-2 justify-between w-[90%] h-12 items-center" +
+                            (m.origin ? " relative isolate z-10 " : "")
+                        }
+                    >
+                        {m.origin && (
+                            <div
+                                aria-hidden
+                                className="pointer-events-none absolute -inset-2 -z-10 bg-radial-[ellipse_80%_40%_at_50%_50%] from-accent/5.5 to-transparent to-85%"
+                            />
+                        )}
                         <span className="font-sans uppercase tracking-[0.15em] text-subtext-1 text-xs">
-                        {m.label}
+                            {m.label}
                         </span>
                         <div className=" w-[85%] flex items-center border-x border-overlay-0 h-full">
                             <span className="h-px w-full bg-overlay-0" />
