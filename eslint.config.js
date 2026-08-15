779f1152ac265bfb7cc54e649eeefb260001f6f7
serenity <98162476+NekoDrone@users.noreply.github.com>
2026-08-15 18:05:31 +0800

    lint: eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -2,35 +2,55 @@
 
 import { tanstackConfig } from "@tanstack/eslint-config";
 
-export default [
-    ...tanstackConfig,
-    {
-        rules: {
-            "import/no-cycle": "off",
-            "import/order": "off",
-            "sort-imports": "warn",
-            "pnpm/json-enforce-catalog": "off",
-            "@typescript-eslint/consistent-type-imports": "error",
-            "@typescript-eslint/array-type": ["error", { default: "generic" }],
-            "no-unused-vars": [
-                "warn",
-                {
-                    args: "all",
-                    argsIgnorePattern: "^_",
-                    varsIgnorePattern: "^_",
-                },
-            ],
-            "@typescript-eslint/no-unused-vars": [
-                "warn",
-                {
-                    args: "all",
-                    argsIgnorePattern: "^_",
-                    varsIgnorePattern: "^_",
-                },
+import js from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
+
+export default defineConfig({
+    files: ["**/*.{js,ts}"],
+    extends: [
+        js.configs.recommended,
+        tseslint.configs.recommended,
+        ...tanstackConfig,
+        {
+            rules: {
+                "import/no-cycle": "off",
+                "import/order": "off",
+                "sort-imports": "warn",
+                "pnpm/json-enforce-catalog": "off",
+                "@typescript-eslint/consistent-type-imports": "error",
+                "@typescript-eslint/array-type": [
+                    "error",
+                    { default: "generic" },
+                ],
+                "no-unused-vars": [
+                    "warn",
+                    {
+                        args: "all",
+                        argsIgnorePattern: "^_",
+                        varsIgnorePattern: "^_",
+                    },
+                ],
+                "@typescript-eslint/no-unused-vars": [
+                    "warn",
+                    {
+                        args: "all",
+                        argsIgnorePattern: "^_",
+                        varsIgnorePattern: "^_",
+                    },
+                ],
+            },
+        },
+        {
+            ignores: [
+                "eslint.config.js",
+                "prettier.config.js",
+                ".tanstack/",
+                ".output/",
+                ".wrangler/",
+                ".import/",
+                ".direnv/",
             ],
         },
-    },
-    {
-        ignores: ["eslint.config.js", "prettier.config.js"],
-    },
-];
+    ],
+});
